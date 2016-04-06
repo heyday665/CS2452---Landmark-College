368c65331d9a018f38cf9a288462cfdffc35d6e4
Jack Dacey <daceyj@clarkson.edu>
2016-04-06 13:02:19 -0400

    Should have functioning voice chat now. Should. Biiiig should. No guarantees.

--- skipee-assign5/src/CALL.java
+++ skipee-assign5/src/CALL.java
@@ -8,21 +8,43 @@ import javax.sound.sampled.*;
 
 public class CALL implements Runnable {
 	Socket clientSock;
-	Thread t;	
+	Thread t;
+	ByteArrayOutputStream out;	
 
 	CALL(Socket s){
 		clientSock = s;
 		t = new Thread(this, "CallThread");
+		t.start();
 	}
 
 	public void run(){
-		//
+		MICCATCH miccatch = new MICCATCH(clientSock);
+		try {
+			final AudioFormat format = getFormat();
+			DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+			final SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+			line.open(format);
+			line.start();
+
+			int bufferSize = (int) format.getSampleRate() * format.getFrameSize();
+			byte buffer[] = new byte[bufferSize];
+
+			final AudioInputStream ais = new AudioInputStream(clientSock.getInputStream(), format, 2000000);
+			int count;
+			while ((count = ais.read(buffer, 0, buffer.length))!=-1) {
+				if (count>0) {
+					line.write(buffer, 0, count);
+				}
+			}
+			line.drain();
+			line.close();
+		} catch (Exception e) {e.printStackTrace(System.out); System.exit(1);}
 	}
 
 	private AudioFormat getFormat() {
 		float sampleRate = 16000;
-		int sampleSizeInBits = 8;
-		int channels = 1;
+		int sampleSizeInBits = 16;
+		int channels = 2;
 		boolean signed = true;
 		boolean bigEndian = false;
 		return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
---
+++ skipee-assign5/src/MICCATCH.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import javax.sound.sampled.*;
+import java.net.*;
+
+public class MICCATCH implements Runnable{
+	Socket clientSock;
+	Thread t;
+
+	MICCATCH(Socket s){
+		clientSock = s;
+		t = new Thread(this, "MicThread");
+		t.start();
+	}
+	
+	public void run(){
+		try {
+			final AudioFormat format = getFormat();
+			DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+			final TargetDataLine line = (TargetDataLine) AudioSystem.getLine(info);
+			line.open(format);
+			line.start();
+
+			int bufferSize = 64;
+			byte buffer[] = new byte[bufferSize];
+
+			BufferedOutputStream out = null;
+			out = new BufferedOutputStream(clientSock.getOutputStream());
+			while (true){
+				int count = line.read(buffer, 0, buffer.length);
+				if (count > 0){
+					out.write(buffer, 0, count);
+					out.flush();
+				}
+			}
+		} catch (Exception e) {e.printStackTrace(System.out);}
+	}
+
+	static AudioFormat getFormat(){
+		float sampleRate = 16000;
+		int sampleSizeInBits = 16;
+		int channels = 2;
+		boolean signed = true;
+		boolean bigEndian = false;
+		return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+	}
+}
